15be7fe755635308646e235d75b724026586e77e
Poiroot <herculepo9@gmail.com>
2021-10-21 21:26:21 +0700

    Latihan Intent Dicoding

---
+++ app/src/main/java/com/poirot/project/myintentapp/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.poirot.project.myintentapp
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnMoveActivity: Button = findViewById(R.id.btn_move_activity)
+        btnMoveActivity.setOnClickListener(this)
+
+        val btnMoveWithDataActivity: Button = findViewById(R.id.btn_move_activity_data)
+        btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_move_activity_object)
+        btnMoveWithObject.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Ewa Arizki Hutama")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 24)
+                startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person(
+                    "Ewa Arizki Hutama",
+                    20,
+                    "academy@dicoding.com",
+                    "Jakarta"
+                )
+
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/poirot/project/myintentapp/MoveActivity.kt
@@ -0,0 +1,11 @@
+package com.poirot.project.myintentapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
